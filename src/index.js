9f6c06b1fdbfee281a505436ef7256d0a2e67fbb
Mark <lookandlearn76@yahoo.co.uk>
2019-12-20 17:31:25 +0000

    updated approvalCard and index.js

--- src/index.js
+++ src/index.js
@@ -2,45 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45pm"
-        commentText="Nice Blog"
-        avatar={faker.image.avatar}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="today at 2pm"
-        commentText="Really nice!"
-        avatar={faker.image.avatar}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45pm"
+          commentText="Nice Blog"
+          avatar={faker.image.avatar}
+        />
+      </ ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="today at 2pm"
+          commentText="Really nice!"
+          avatar={faker.image.avatar}
+        />
+      </ApprovalCard>
       <CommentDetail
         author="Jane"
         timeAgo="today at 1:30pm"
         commentText="Lovely"
         avatar={faker.image.avatar}
       />
-      <CommentDetail
-        author="Phil"
-        timeAgo="today at 1pm"
-        commentText="Super!"
-        avatar={faker.image.avatar}
-      />
-      <CommentDetail
-        author="Rob"
-        timeAgo="today at 12pm"
-        commentText="Hate it!"
-        avatar={faker.image.avatar}
-      />
-      <CommentDetail
-        author="Bill"
-        timeAgo="today at 10am"
-        commentText="Yuck!"
-        avatar={faker.image.avatar}/>
+      <ApprovalCard>
+        <CommentDetail
+          author="Phil"
+          timeAgo="today at 1pm"
+          commentText="Super!"
+          avatar={faker.image.avatar}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Rob"
+          timeAgo="today at 12pm"
+          commentText="Hate it!"
+          avatar={faker.image.avatar}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Bill"
+          timeAgo="today at 10am"
+          commentText="Yuck!"
+          avatar={faker.image.avatar}
+        />
+      </ApprovalCard>
     </div>
   );
 };
