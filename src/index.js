6687ff30286ff5916b4b255357efe3b01a8ad4c6
Mark <lookandlearn76@yahoo.co.uk>
2019-12-20 15:19:20 +0000

    commentDetail props setup for text, avatar and time

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45pm"
+        commentText="Nice Blog"
+        avatar={faker.image.avatar}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="today at 2pm"
+        commentText="Really nice!"
+        avatar={faker.image.avatar}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="today at 1:30pm"
+        commentText="Lovely"
+        avatar={faker.image.avatar}
+      />
+      <CommentDetail
+        author="Phil"
+        timeAgo="today at 1pm"
+        commentText="Super!"
+        avatar={faker.image.avatar}
+      />
+      <CommentDetail
+        author="Rob"
+        timeAgo="today at 12pm"
+        commentText="Hate it!"
+        avatar={faker.image.avatar}
+      />
+      <CommentDetail
+        author="Bill"
+        timeAgo="today at 10am"
+        commentText="Yuck!"
+        avatar={faker.image.avatar}/>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'))
